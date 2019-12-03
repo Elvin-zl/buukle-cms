3400b1af9a662c78edafb09ef9578cb8d34ba5ce
zhanglei1102 <zhanglei1102@jd.com>
2019-12-03 18:22:57 +0800

    解除 buukle-util ，buukle-common包的依赖传递 ，更新各模块的pom

--- buukle-cms-web/src/main/java/top/buukle/cms/web/CmsApplication.java
+++ buukle-cms-web/src/main/java/top/buukle/cms/web/CmsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
-@MapperScan({"top.buukle.cms.dao","top.buukle.util.mvc"})
+@MapperScan({"top.buukle.cms.dao","top.buukle.common.mvc"})
 @SpringBootApplication(scanBasePackages={"top.buukle.cms.*"})
 @EnableFeignClients(basePackages = {"top.buukle.*"})
 @EnableRedisHttpSession
